29c747b6a08e589d2cfbcad4b67448840bf02f5c
wangshukai <wangshukai@mengniu.cn>
2023-09-12 16:51:52 +0800

    修改依赖包版本

--- test/TestSdk.php
+++ test/TestSdk.php
@@ -36,15 +36,6 @@ class TestSdk extends Testcase
      */
     public function testDip()
     {
-/*        $config = ['clientId' => 'a3a9efeb-1825-40d9-a9ef-eb182580d9e5', 'secret' => '905C0F7E-5607-43A2-9C0F-7E5607E3A236', 'gatewayUrl' => "https://t-openapi-gateway.mengniu.cn"];
-        $openApiSdk = new OpenApiSdk($config);
-        $item = $openApiSdk->CommonRequest;
-        $data = [
-            'messageHeader' => ["messageId"=>"0e9f163bd55849679586e96e70ce7da0",
-                "interfacePath"=>"getStoreInfo","sendTime"=>"2023-04-11 16:06:27","sender"=>"mn","receiver"=>"cts"],"data"=>["storeCode"=>"5600042964","businessType"=>11]
-        ];
-        $res=$item->commonReq("/store-center/api/getStoreInfo",$data,"POST");//查询用户信息
-        var_dump($res);*/
         $Dispatch = new Dispatch(CommonRequest::class, ["debug"=>false,'clientId' => 'xxxx', 'secret' => 'xxx', 'gatewayUrl' => "xxx"]);
         $data = [
             'messageHeader' => ["messageId"=>"0e9f163bd55849679586e96e70ce7da0",
